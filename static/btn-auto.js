87695f108b0be232737cb0ba1f0567bb9c01cb69
Mariella_Vouk <Mariellavoukprestes@gmail.com>
2024-08-04 23:21:43 -0300

    primeiro commit

---
+++ static/btn-auto.js
@@ -0,0 +1,19 @@
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    prox_img()
+}, 5000)
+
+function prox_img(){
+    cont++ 
+    if(cont > 3) {
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+
+}
